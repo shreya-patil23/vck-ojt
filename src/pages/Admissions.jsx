61fa68ee10612405488b73254a0c89f277158a0b
Shreya Patil <sp9968373@gmail.com>
2025-06-10 13:28:23 +0530

    third commit

--- src/pages/Admissions.jsx
+++ src/pages/Admissions.jsx
@@ -6,42 +6,44 @@ const Admissions = () => {
     return (
         <div>
             <Header/>
-             <div>
-        <h1>Admissions at Vivekanand College</h1>
+             <div id="p">
+        <h1 id="q">Admissions at Vivekanand College</h1>
         <p>Welcome to the Admissions page of Vivekanand College! We are delighted you're considering joining our vibrant academic community. Our admission process is designed to be straightforward and accessible.</p>
         <p>Please review the eligibility criteria and key dates below for the upcoming academic year. For any queries, feel free to contact our admissions office.</p>
+        <div>
         <h1>Eligibility Criteria for Undergraduate Programs</h1>
-        <table id="p">
+        <table id="r">
              <thead>
-                <tr id="p">
-                    <th id="p">Program</th>
-                    <th id="p">Minimum Qualification</th>
-                    <th id="p">Required Subjects</th>
-                    <th id="p">Minimum Marks (%)</th>
+                <tr>
+                    <th>Program</th>
+                    <th>Minimum Qualification</th>
+                    <th>Required Subjects</th>
+                    <th>Minimum Marks (%)</th>
                 </tr>
              </thead>
              <tbody>
-                <tr id="p">
-                    <tb id="p">B.Sc. Computer Science	</tb>
-                    <tb id="p">10+2 (or equivalent)</tb>
-                    <tb id="p">Physics, Chemistry, Maths</tb>
-                    <tb id="p">50%</tb>
+                <tr>
+                    <tb>B.Sc. Computer Science	</tb>
+                    <tb>10+2 (or equivalent)</tb>
+                    <tb>Physics, Chemistry, Maths</tb>
+                    <tb id="u">50%</tb>
                 </tr>
-                <tr id="p">
-                    <tb id="p">B.Com. Accounting & Finance</tb>
-                    <tb id="p">10+2 (or equivalent)</tb>
-                    <tb id="p">Commerce Stream</tb>
-                    <tb id="p">45%</tb>
+                <tr>
+                    <tb>B.Com. Accounting & Finance</tb>
+                    <tb>10+2 (or equivalent)</tb>
+                    <tb>Commerce Stream</tb>
+                    <tb id="u">45%</tb>
                 </tr>
-                <tr id="p">
-                    <tb id="p">B.A. English Literature	</tb>
-                    <tb id="p">10+2 (or equivalent)</tb>
-                    <tb id="p">Any Stream</tb>
-                    <tb id="p">40%</tb>
+                <tr>
+                    <tb>B.A. English Literature	</tb>
+                    <tb>10+2 (or equivalent)</tb>
+                    <tb>Any Stream</tb>
+                    <tb id="u">40%</tb>
                 </tr>
              </tbody>
 
         </table>
+        </div>
        
        <h1>Application Process</h1>
        <ol>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,9 +12,8 @@ const HomePage = () => {
             <div id="C"> 
                <h1 id="A">Welcome To Vivekanand College!</h1>
                <p id="A">Your Journey To excellence Start Here.</p>
-               <a href="/Admissions">
-               <button id="B">Apply Now!</button>
-               </a>
+               <Link to='/admissions'><button id="x">Apply Now!</button> </Link>
+                               
             </div>
           <p>**Vivekanand College is a premier educational institution dedicated to fostering academic excellence, innovation, and holistic development. Established in [Year of Establishment, e.g., 1980], we have proudly served generations of students, empowering them to achieve their full potential.</p>
           <p>At Vivekanand College, we believe in a vibrant learning environment that extends beyond textbooks. Our state-of-the-art facilities, experienced faculty, and diverse student community create a unique ecosystem where curiosity thrives and future leaders are shaped.</p>
